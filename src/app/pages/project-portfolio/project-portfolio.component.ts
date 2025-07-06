8c04e391e2502ce429336d35cafe7b1901fedee0
Tungky Adi Pradana <tungky.adi@uii.ac.id>
2025-07-06 08:09:57 +0700

    initial setup and add some pages component

---
+++ src/app/pages/project-portfolio/project-portfolio.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-project-portfolio',
+  imports: [],
+  templateUrl: './project-portfolio.component.html',
+  styleUrl: './project-portfolio.component.css'
+})
+export class ProjectPortfolioComponent {
+
+}
---
+++ src/app/pages/team-member/team-member.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-team-member',
+  imports: [],
+  templateUrl: './team-member.component.html',
+  styleUrl: './team-member.component.css'
+})
+export class TeamMemberComponent {
+
+}
