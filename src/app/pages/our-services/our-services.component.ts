1f19d664afc67cfc3b1b2b4797799299891018b6
Tungky Adi Pradana <tungky.adi@uii.ac.id>
2025-08-15 14:28:16 +0700

    add more assets and update our services page

--- src/app/pages/our-services/our-services.component.ts
+++ src/app/pages/our-services/our-services.component.ts
@@ -1,9 +1,13 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from '../../shared/header/header.component';
+import { FooterComponent } from '../../shared/footer/footer.component';
 
 @Component({
   selector: 'app-our-services',
-  imports: [HeaderComponent],
+  imports: [
+    HeaderComponent,
+    FooterComponent
+  ],
   templateUrl: './our-services.component.html',
   styleUrl: './our-services.component.css'
 })
