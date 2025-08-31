e01edb1792c3f34c52c546cdfe3549e9c90d2ae0
Tungky Adi Pradana <tungky.adi@uii.ac.id>
2025-08-31 22:46:26 +0700

    update content for our services

--- src/app/pages/our-services/our-services.component.ts
+++ src/app/pages/our-services/our-services.component.ts
@@ -1,12 +1,14 @@
 import { Component, OnInit } from '@angular/core';
 import { HeaderComponent } from '../../shared/header/header.component';
 import { FooterComponent } from '../../shared/footer/footer.component';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-our-services',
   imports: [
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    MatIconModule
   ],
   templateUrl: './our-services.component.html',
   styleUrl: './our-services.component.css'
