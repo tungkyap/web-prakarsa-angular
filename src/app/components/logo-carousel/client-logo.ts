68a69052cdef818e5598099321d30dc6ef0a5c80
Tungky Adi Pradana <tungky.adi@uii.ac.id>
2025-09-19 21:58:24 +0700

    add assets for clients logo

---
+++ src/app/components/logo-carousel/client-logo.ts
@@ -0,0 +1,76 @@
+export interface Client {
+  name?: string,
+  url?: string,
+  isHighlighted?: boolean,
+}
+
+export let client: Client[] = [
+  {
+    name: 'Brantas Abipraya',
+    url: '/images/clients/abipraya_bumn_logo.png'
+  },
+  {
+    name: 'PT Aneka Pertambangan TBK',
+    url: '/images/clients/antam_logo.png'
+  },
+  {
+    name: 'AstraWorld',
+    url: '/images/clients/astraworld_logo.png'
+  },
+  {
+    name: 'Bandara Internasional Batam',
+    url: '/images/clients/bandara_internasional_batam_logo.webp'
+  },
+  {
+    name: 'PT Cipta Graha Kanaka',
+    url: '/images/clients/asanka_logo.webp'
+  },
+  {
+    name: 'Front One Hotel',
+    url: '/images/clients/frontone_hotel_logo.png'
+  },
+  {
+    name: 'PT Inti Beton',
+    url: '/images/clients/intibeton_logo.webp'
+  },
+  {
+    name: 'JAMKRINDO',
+    url: '/images/clients/jamkrindo_logo.png'
+  },
+  {
+    name: 'PT LPP Agro Nusantara',
+    url: '/images/clients/lpp_agro_logo.png'
+  },
+  {
+    name: 'Pemerintah Kota Magelang',
+    url: '/images/clients/magelang_kota_logo.png'
+  },
+  {
+    name: 'Manohara Hotel Yogyakarta',
+    url: '/images/clients/manohara_yk_logo.png'
+  },
+  {
+    name: 'PT Multi Niaga Nusantara Indonesia',
+    url: '/images/clients/multi_niaga_nusantara_logo.png'
+  },
+  {
+    name: 'PT Kereta Api Indonesia',
+    url: '/images/clients/pt_kai_logo.png'
+  },
+  {
+    name: 'Kementerian Pekerjaan Umum dan Perumahan Rakyat',
+    url: '/images/clients/pupr_logo.png'
+  },
+  {
+    name: 'SMP IT Salman Al Farisi',
+    url: '/images/clients/smp_it_saf_logo.jpg'
+  },
+  {
+    name: 'STMM MMTC',
+    url: '/images/clients/stmm_mmtc_logo.png'
+  },
+  {
+    name: 'Universitas Gadjah Mada',
+    url: '/images/clients/ugm_horizontal_logo.png'
+  },
+]
