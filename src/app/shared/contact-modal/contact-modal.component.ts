d8670aa764a3102523bf778cace0e058bd967c95
Tungky Adi Pradana <tungky.adi@uii.ac.id>
2025-09-19 15:15:56 +0700

    Add modal contact detail

--- src/app/shared/contact-modal/contact-modal.component.ts
+++ src/app/shared/contact-modal/contact-modal.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { DialogRef } from '@angular/cdk/dialog';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-contact-modal',
@@ -7,5 +9,17 @@ import { Component } from '@angular/core';
   styleUrl: './contact-modal.component.css'
 })
 export class ContactModalComponent {
+  constructor(private router: Router) {}
 
+  private dialogRef = inject(DialogRef, { optional: true });
+  protected closeModal() {
+    this.dialogRef?.close();
+  }
+
+  protected redirectToContactUs() {
+    this.dialogRef?.close();
+    this.router.navigate(['/contact-us']).then(() => {
+      window.scroll(0,0);
+    });
+  }
 }
