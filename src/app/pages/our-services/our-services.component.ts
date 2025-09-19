124b65dbacdb8ee6c9e501ffeff67e38a4ec9779
Tungky Adi Pradana <tungky.adi@uii.ac.id>
2025-09-19 10:22:08 +0700

    add scroll to top button

--- src/app/pages/our-services/our-services.component.ts
+++ src/app/pages/our-services/our-services.component.ts
@@ -1,19 +1,33 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, HostListener, OnInit } from '@angular/core';
 import { HeaderComponent } from '../../shared/header/header.component';
 import { FooterComponent } from '../../shared/footer/footer.component';
 import { MatIconModule } from '@angular/material/icon';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-our-services',
   imports: [
     HeaderComponent,
     FooterComponent,
+    CommonModule,
     MatIconModule
   ],
   templateUrl: './our-services.component.html',
   styleUrl: './our-services.component.css'
 })
 export class OurServicesComponent implements OnInit {
+  showScrollTop = false;
+
+  // detect scroll position
+  @HostListener('window:scroll', [])
+  onWindowScroll() {
+    this.showScrollTop = window.scrollY > 300; // show after 300px scroll
+  }
+
+  // scroll smoothly to top
+  scrollToTop() {
+    window.scrollTo({ top: 0, behavior: 'smooth' });
+  }
 
   ngOnInit(): void {
     window.scroll(0,0);
