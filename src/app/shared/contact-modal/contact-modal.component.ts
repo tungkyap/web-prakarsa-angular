b4567ee285a38ca0c58c52cdc4be0739bca63d5a
Tungky Adi Pradana <tungky.adi@uii.ac.id>
2025-09-19 10:29:33 +0700

    add contact modal popup component on shared component

---
+++ src/app/shared/contact-modal/contact-modal.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-contact-modal',
+  imports: [],
+  templateUrl: './contact-modal.component.html',
+  styleUrl: './contact-modal.component.css'
+})
+export class ContactModalComponent {
+
+}
